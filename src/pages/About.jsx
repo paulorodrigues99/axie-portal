4fb65afa52154e6ce73f5ab1e49a9f02a3bdcfe0
Paulo Rodrigues <paulorgomes99@gmail.com>
2021-08-27 10:29:39 -0300

    feature/about-page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,40 @@
 // Sample card from Airbnb
 
 import { StarIcon } from "@chakra-ui/icons";
-import { Badge, Box, Image } from "@chakra-ui/react";
+import { Badge, Box, Heading, Image } from "@chakra-ui/react";
+import strategy from "../../public/asset/arquitetura-1.png";
+import {
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+} from "@chakra-ui/react";
 
 function AirbnbExample() {
+  const imgurl =
+    "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/0d34cb32-1f72-479d-8f8c-38a7c8c5f341/arquitetura-1.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210827%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210827T130610Z&X-Amz-Expires=86400&X-Amz-Signature=1607f622977f9ef877a710008b8184e8ef82e5a0d41da5c6696fe43da6dbaf24&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22arquitetura-1.png%22";
   const property = {
-    imageUrl: "https://bit.ly/2Z4KKcF",
+    imageUrl: imgurl,
     imageAlt: "Rear view of modern home with pool",
-    beds: 3,
-    baths: 2,
-    title: "Modern home in city center in the heart of historic Los Angeles",
-    formattedPrice: "$1,900.00",
-    reviewCount: 34,
-    rating: 4,
+    title: "Histórias de usuário",
   };
-
   return (
-    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+    <Box
+      as="section"
+      maxW="1440px"
+      py="4"
+      px={[4, 6, 10, 14, 20]}
+      mx="auto"
+      display="flex"
+      flexDir={{ base: "column-reverse", lg: "column" }}
+      justifyContent={{ base: "center", lg: "space-between" }}
+      alignItems="center"
+      textAlign={{ base: "center", lg: "left" }}
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+    >
       <Image src={property.imageUrl} alt={property.imageAlt} />
 
       <Box p="6">
@@ -24,16 +42,6 @@ function AirbnbExample() {
           <Badge borderRadius="full" px="2" colorScheme="teal">
             New
           </Badge>
-          <Box
-            color="gray.500"
-            fontWeight="semibold"
-            letterSpacing="wide"
-            fontSize="xs"
-            textTransform="uppercase"
-            ml="2"
-          >
-            {property.beds} beds &bull; {property.baths} baths
-          </Box>
         </Box>
 
         <Box
@@ -45,30 +53,54 @@ function AirbnbExample() {
         >
           {property.title}
         </Box>
-
-        <Box>
-          {property.formattedPrice}
-          <Box as="span" color="gray.600" fontSize="sm">
-            / wk
-          </Box>
-        </Box>
-
-        <Box d="flex" mt="2" alignItems="center">
-          {Array(5)
-            .fill("")
-            .map((_, i) => (
-              <StarIcon
-                key={i}
-                color={i < property.rating ? "teal.500" : "gray.300"}
-              />
-            ))}
-          <Box as="span" ml="2" color="gray.600" fontSize="sm">
-            {property.reviewCount} reviews
-          </Box>
+        <Box mt="1" fontWeight="normal" as="h4" lineHeight="tight" isTruncated>
+          <Heading as="h2" size="2xl">
+            Jogador
+          </Heading>
+          <br></br>
+          <List spacing={2}>
+            <ListItem>
+              Eu como um usuário quero ter acesso a plataforma por meio de login
+              e senha
+            </ListItem>
+            <ListItem>Eu como um aluno desejo alugar uma conta</ListItem>
+            <ListItem>
+              Eu como usuário quero ter disponível pra mim um status de
+              aprovação do meu login no jogo.
+            </ListItem>
+            <ListItem>
+              Eu como usuário desejo ter acesso a um contrato virtual garantindo
+              meus serviços
+            </ListItem>
+          </List>
+          <br></br>
+          <Heading as="h2" size="2xl">
+            Administrador
+          </Heading>
+          <br></br>
+          <List spacing={2}>
+            <ListItem>
+              Eu como um administrador quero ter acesso a plataforma por meio de
+              login e senha
+            </ListItem>
+            <ListItem>
+              Eu como usuário quero ter disponível pra mim um status de
+              aprovação dos logins inscritos nas minhas contas.
+            </ListItem>
+            <ListItem>
+              Eu como um usuário administrador quero cadastrar contas Eu como
+              usuário quero ter disponível pra mim um status de aprovação do meu
+              login no jogo.
+            </ListItem>
+            <ListItem>
+              Eu como usuário administrador desejo ter acesso a um contrato
+              virtual garantindo meus serviços
+            </ListItem>
+          </List>
         </Box>
       </Box>
     </Box>
   );
 }
 
-export default AirbnbExample
+export default AirbnbExample;
